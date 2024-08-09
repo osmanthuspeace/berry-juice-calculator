0ebda8d94d7d69df9adbaa56d3e4ca6c0edb8645
OsmanthusPeace <146796167+osmanthuspeace@users.noreply.github.com>
2024-08-09 11:56:26 +0800

    Master (#1)
    * init: draw the ui and config the electron
    
    * update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # berry-juice-calculator
+
+用于计算机专业学习的计算器，支持二进制位键盘，旨在解决macOS自带的计算器无法放大的问题
+
+使用electron+原生js开发
---
+++ preload.js
@@ -0,0 +1,11 @@
+// preload.js
+const {contextBridge, ipcRenderer} = require('electron');
+
+contextBridge.exposeInMainWorld('versions', {
+    node: () => process.versions.node,
+    chrome: () => process.versions.chrome,
+    electron: () => process.versions.electron,
+    // 除函数之外，我们也可以暴露变量
+    ping: () => ipcRenderer.invoke('ping')
+
+})
